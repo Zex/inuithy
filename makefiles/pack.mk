0c68282792c3d87d8cf2c72babfa197fb30effea
Zex Li <top_zlynch@yahoo.com>
2016-10-19 23:04:14 +0800

    add makefiles

---
+++ makefiles/pack.mk
@@ -0,0 +1,32 @@
+## Create package
+# Author: Zex Li <top_zlynch@yahoo.com>
+#
+ifndef PROJECT
+$(error "PROJECT not defined")
+endif
+
+ifndef VERSION
+$(error "VERSION not defined")
+endif
+
+ifndef VERSION_PATH
+$(error "VERSION_PATH not defined")
+endif
+
+OUTPUT_TAR	           := $(strip $(PROJECT))-$(strip $(VERSION)).tar.bz2
+OUTPUT_TAR_SOURCE	   := .
+OUTPUT_TAR_PATH        := $(BUILD)/$(OUTPUT_TAR)
+OUTPUT_TAR_EXCLUDES	   := $(BUILD) *.pyc .git .svn .*.swp docs *~
+
+$(OUTPUT_TAR_PATH): $(BUILD) $(VERSION_PATH)
+	$(ECHO) "---------------Creating $@------------------"
+	$(TAR) cfj $(OUTPUT_TAR_PATH) $(OUTPUT_TAR_SOURCE) $(OUTPUT_TAR_EXCLUDES:%=--exclude=%) --total
+
+$(VERSION_PATH):
+	$(ECHO) "---------------Creating $@------------------"
+	$(ECHO) "## AUTO GENERATED ON `date`" > $@
+	$(ECHO) "" >> $@
+	$(ECHO) "INUITHY_VERSION = \""$(VERSION)"\"" >> $@
+	$(ECHO) "" >> $@
+
+
---
+++ makefiles/predef.mk
@@ -0,0 +1,14 @@
+## Predefinition for make
+# Author: Zex Li <top_zlynch@yahoo.com>
+#
+ECHO		:= @echo
+TAR			:= tar
+FIND		:= find
+RM			:= rm -rf
+MKDIR		:= mkdir -p
+
+
+BUILD		           := build
+VERSION_PATH           := inuithy/common/version.py
+
+
