771d6d840a69b1833a11563969daf28a7016d968
Zex Li <top_zlynch@yahoo.com>
2016-11-01 00:48:07 +0800

    change insert format

--- inuithy/storage/mongodb_plugin.py
+++ inuithy/storage/mongodb_plugin.py
@@ -3,6 +3,8 @@
 #
 from datetime import datetime as dt
 from inuithy.common.predef import *
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class MongodbStorage:
     """MongoDB adapter
@@ -18,57 +20,168 @@ class MongodbStorage:
     @cli.setter
     def cli(self, val): pass
 
-    def __init__(self, host, port) 
-        super(MongodbStorage, self).__init__("MongoDB", host, port)
+    @property
+    def storage_name(self): return self.__storage_name
+    @storage_name.setter
+    def storage_name(self, val): pass
+
+    @property
+    def host(self): return self.__host
+    @host.setter
+    def host(self, val): pass
+
+    @property
+    def port(self): return self.__port
+    @port.setter
+    def port(self, val): pass
+
+    @property
+    def localpath(self): return None
+    @localpath.setter
+    def localpath(self, val): pass
+
+    @property
+    def storage_path(self):
+        return self.localpath or string_write("{}:{}", self.host, self.port)
+    @storage_path.setter
+    def storage_path(self, val): pass
+
+    @property
+    def db(self): return self.__db
+    @db.setter
+    def db(self, val): pass
+
+    @property
+    def trafrec(self): return self.__coll_trafrec
+    @trafrec.setter
+    def trafrec(self, val): pass
+
+    def __init__(self, host, port): 
+        self.__storage_name = StorageType.MongoDB.name
+        self.__host = host
+        self.__port = int(port)
         self.__cli = MongoClient(self.host, self.port)
-        self.db = self.cli.inuithy_traffic_records
+        self.__db = self.cli.inuithy
+        self.__coll_trafrec = self.db.traffic_records
    
-    
-    def __del__(self):
+    def close(self):
         self.cli.close()
 
     def __str__(self):
-        return str(string_write("cli:{} host:{} port:{} name:{}", self.__cli, self.host, self.port, self.storage_name)
+        return str(string_write("cli:{} host:{} port:{} name:{}", self.__cli, self.host, self.port, self.storage_name))
 
     def insert_config(self, data):
         """
-        data
+        Storage structure
         - CFGKW_GENID      : <connect configure and records> => string
         - CFGKW_TARGET_NWLAYOUT : <network layout>  => dict
         - CFGKW_TARGET_TRAFFIC  : <traffic layout>  => dict
         - CFGKW_RECORDS      : [                    => list
-        - CFGKW_GENID       : <connect configure and records> => string
-        -- CFGKW_TIME        : <datetime string>            => string +
-        -- CFGKW_MSG         : <string from/to serial port> => string | 
-        -- CFGKW_CLIENTID    : <agentid>             => string        |
-        -- CFGKW_HOST        : <agent host>          => string        +->record
-        -- CFGKW_SENDER      : <sender node addr>    => string        |
-        -- CFGKW_RECIPIENT   : <recipient node addr> => string        |
-        -- CFGKW_PKGSIZE     : <package size>        => integer-------+
+        -- CFGKW_TIME        : <recored datetime string>      => string---+
+        -- CFGKW_MSG         : <string from/to serial port>   => string   | 
+        -- CFGKW_CLIENTID    : <agentid>             => string            |
+        -- CFGKW_HOST        : <agent host>          => string            +->record
+        -- CFGKW_SENDER      : <sender node addr>    => string            |
+        -- CFGKW_RECIPIENT   : <recipient node addr> => string            |
+        -- CFGKW_PKGSIZE     : <package size>        => integer-----------+
            ]
+        Data sample - initial state
+        - CFGKW_TARGET_NWLAYOUT : <network layout>  => dict
+        - CFGKW_TARGET_TRAFFIC  : <traffic layout>  => dict
+        - CFGKW_RECORDS         : []                => list
+        Return
+        - CFGKW_GENID           : <connect configure and records> => string
         """
-        self.__cli.inser_one(data)
+        data[CFGKW_RECORDS]     = []
+        return str(self.__coll_trafrec.insert_one(data).inserted_id)
 
     def insert_record(self, data):
         """
+        Storage structure
         - CFGKW_GENID      : <connect configure and records> => string
         - CFGKW_TARGET_NWLAYOUT : <network layout>  => dict
         - CFGKW_TARGET_TRAFFIC  : <traffic layout>  => dict
         - CFGKW_RECORDS      : [                    => list
-        -- CFGKW_GENID      : <connect configure and records> => string
-        -- CFGKW_TIME        : <recored datetime string>       => string +
-        -- CFGKW_MSG         : <string from/to serial port> => string | 
-        -- CFGKW_CLIENTID    : <agentid>             => string        |
-        -- CFGKW_HOST        : <agent host>          => string        +->record
-        -- CFGKW_SENDER      : <sender node addr>    => string        |
-        -- CFGKW_RECIPIENT   : <recipient node addr> => string        |
-        -- CFGKW_PKGSIZE     : <package size>        => integer-------+
+        -- CFGKW_TIME        : <recored datetime string>      => string---+
+        -- CFGKW_MSG         : <string from/to serial port>   => string   | 
+        -- CFGKW_MSG_TYPE    : Message type          => string   | 
+        -- CFGKW_CLIENTID    : <agentid>             => string            |
+        -- CFGKW_HOST        : <agent host>          => string            +->record
+        -- CFGKW_SENDER      : <sender node addr>    => string            |
+        -- CFGKW_RECIPIENT   : <recipient node addr> => string            |
+        -- CFGKW_PKGSIZE     : <package size>        => integer-----------+
            ]
+        Data sample - on record
+        {
+        -- CFGKW_GENID       : <connect configure and records> => string--+
+        -- CFGKW_MSG         : <string from/to serial port>   => string   | 
+        -- CFGKW_CLIENTID    : <agentid>             => string            |
+        -- CFGKW_HOST        : <agent host>          => string            +->record
+        -- CFGKW_SENDER      : <sender node addr>    => string            |
+        -- CFGKW_RECIPIENT   : <recipient node addr> => string            |
+        -- CFGKW_PKGSIZE     : <package size>        => integer-----------+
+        }
+        Additional field
+        -- CFGKW_TIME        : <recored datetime string>      => string
         """
-        data[CFGKW_TIME] = datatime.now() 
-#        self.__cli.insert_one(data)
-        self.__cli.
+        data[CFGKW_TIME] = str(dt.now())
+        self.trafrec.update(
+            {"_id": ObjectId(data[CFGKW_GENID])},
+            {'$push': {CFGKW_RECORDS: data}})
 
+# -----------------------------------------------------------------------------------------
+
+def update_test():
+
+    sto = MongodbStorage('127.0.0.1', 19713)
+    print(sto.trafrec)
+    for r in sto.trafrec.find():
+        oid = r.get('_id')
+        print("------------------------{}---------------------".format(oid))
+        print(r)
+        rec = {
+            CFGKW_GENID:    str(r.get('_id')),
+            CFGKW_MSG:      "lignton 1122",
+            CFGKW_CLIENTID: "HAHAHAH333111MARS",
+            CFGKW_HOST:     "PLUTO",
+            CFGKW_SENDER:   "1111",
+            CFGKW_RECIPIENT:"1122",
+            CFGKW_PKGSIZE:  "10",
+        }
+        sto.insert_record(rec)
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        print(sto.trafrec.find_one({"_id": ObjectId(oid)}))
+
+def check_records():
+    sto = MongodbStorage('127.0.0.1', 19713)
+    print(sto.trafrec)
+    for r in sto.trafrec.find():
+        oid = r.get('_id')
+        print("------------------------{}---------------------".format(oid))
+        print(r)
+
+def check_recv(host, port, genid):
+    sto = MongodbStorage(host, port)
+    for r in sto.trafrec.find({
+            "_id": ObjectId(genid),
+            #"records": { "$elemMatch": {"msgtype": "RECV"} }
+        }):
+        [print(v) for v in r[CFGKW_RECORDS] if v[CFGKW_MSG_TYPE] == MessageType.RECV.name]
+
+def check_sent(host, port, genid):
+    sto = MongodbStorage(host, port)
+    for r in sto.trafrec.find({
+            "_id": ObjectId(genid),
+        }):
+        [print(v) for v in r[CFGKW_RECORDS] if v[CFGKW_MSG_TYPE] == MessageType.SENT.name]
+
+def cleanup():
+    sto = MongodbStorage('127.0.0.1', 19713)
+    sto.trafrec.delete_many({})
 
 if __name__ == '__main__':
-    
+#    check_records() 
+#    check_fields()
+#    cleanup()
+    check_recv('127.0.0.1', 19713, '58176647362ac72cd0778fe5')
+    check_sent('127.0.0.1', 19713, '58176647362ac72cd0778fe5')
--- inuithy/storage/storage.py
+++ inuithy/storage/storage.py
@@ -1,31 +1,36 @@
 ## High-level Storage definition
 # Author: Zex Li <top_zlynch@yahoo.com>
 #
-from inuithy.storage.mongodb_plugin import MongodbStorge
+import logging
+import logging.config as lconf
+from inuithy.common.predef import *
+from inuithy.storage.mongodb_plugin import MongodbStorage
 from abc import ABCMeta, abstractmethod
 
 lconf.fileConfig(INUITHY_LOGCONFIG)
-logger = logging.getLogger('InuithyStorage')
+lg = logging.getLogger('InuithyStorage')
 
 class Storage:
-
+    """High-level storage
+    @trafrec    Traffic records
+    """
     @property
-    def storage_name(self): return self.__storage_name
+    def storage_name(self): return self.__dbplugin.storage_name
     @storage_name.setter
     def storage_name(self, val): pass
 
     @property
-    def host(self): return self.__host
+    def host(self): return self.__dbplugin.host   
     @host.setter
     def host(self, val): pass
 
     @property
-    def port(self): return self.__port
+    def port(self): return self.__dbplugin.port
     @port.setter
     def port(self, val): pass
 
     @property
-    def localpath(self): return self.__localpath
+    def localpath(self): return self.__dbplugin.localpath
     @localpath.setter
     def localpath(self, val): pass
 
@@ -35,21 +40,24 @@ class Storage:
     @storage_path.setter
     def storage_path(self, val): pass
 
-    def __init__(self, tcfg, lg=None)
+    @property
+    def trafrec(self): return self.__dbplugin.trafrec
+    @trafrec.setter
+    def trafrec(self, val): pass
+
+    def __init__(self, tcfg, lg=None):
         self.tcfg = tcfg
-        self.__host = host   
-        self.__port = port
-        self.__localpath = localpath   
         if lg == None: self.lg = logging
         else: self.lg = lg
         self.load_plugin(*self.tcfg.storagetype)
 
     def load_plugin(self, sttype, stname):
+        self.lg.info(string_write("Load plugin: {}:{}", sttype, stname))
         s = self.tcfg.config[CFGKW_TRAFFIC_STORAGE]
         if sttype == TrafficStorage.DB.name:
             if stname == StorageType.MongoDB.name:
-                self.__dbplugin = MongodbPlugin(
-                    *(s[CFGKW_PATH]split(":"))
+                self.__dbplugin = MongodbStorage(
+                    *(s[CFGKW_PATH].split(":"))
                 ) 
         else: #TODO TrafficStorage.FILE.name
             pass
@@ -57,13 +65,24 @@ class Storage:
     def insert_record(self, data):
         """Insert one traffic record
         @data Traffic data, dict
+        @return Inserted id
         """
-        self.__dbplugin.insert_record(data)
+        try:
+            self.__dbplugin.insert_record(data)
+        except Exception as ex:
+            self.lg.error(string_write("Insert record failed: {}", ex))
 
     def insert_config(self, data):
         """Insert current configure
         @data Configure data, dict
         """
-
-        self.__dbplugin.insert_config(data)
+        try:
+            return self.__dbplugin.insert_config(data)
+        except Exception as ex:
+            self.lg.error(string_write("Insert record failed: {}", ex))
+            return None
+    
+    def close(self):
+        self.lg.info("Close storage")
+        self.__dbplugin.close()
 
