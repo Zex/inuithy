8663d550dc03e58a0ec24c0c50880bd792de8e24
Zex Li <top_zlynch@yahoo.com>
2016-11-16 17:07:02 +0800

    backup zigbee proto

--- inuithy/protocol/zigbee_protocol.py
+++ inuithy/protocol/zigbee_protocol.py
@@ -12,4 +12,24 @@ class ZigbeeProtocol(Protocol):
     def joinnw(self, ch):
         return string_write("join {}", ch)
 
+    def writeattribute2(self, destination, packet_size, response=False):
+        self.nr_messages_requested += 1
+        send_request = {}
+        send_request['time'] = time.time()
+        send_request['type'] = 'snd_req'
+        send_request['zbee_nwk_src'] = self.get_network_address()
+        send_request['zbee_nwk_dst'] = destination
+        if response == True:
+            rsp = "1"
+            send_request['ack'] = 'y'
+        else:
+            rsp = "0"
+            send_request['ack'] = 'n'
+
+        msg = 'writeAttribute2  s '+str(destination)+' 20 0 4 0x42 "1" %s '%str(packet_size) + rsp +"\r"
+
+#        self.log_queue.put(send_request)
+#        self.sport.write(msg)
+
+        return msg
 
