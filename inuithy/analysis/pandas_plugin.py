820eb4cc1bb4e5ad3fbd40920812b8136640e008
Zex Li <top_zlynch@yahoo.com>
2016-11-17 16:52:21 +0800

    rename to dest/src

--- inuithy/analysis/pandas_plugin.py
+++ inuithy/analysis/pandas_plugin.py
@@ -4,7 +4,7 @@
 from inuithy.common.predef import INUITHY_LOGCONFIG, INUITHY_CONFIG_PATH,\
 MessageType, T_HOST, StorageType, T_NODE, T_MSG, T_TRAFFIC_TYPE,\
 T_RECORDS, T_MSG_TYPE, string_write, T_TIME, T_GENID, T_CLIENTID,\
-T_SENDER, T_RECIPIENT, T_PKGSIZE, T_REPORTDIR, T_PATH, T_GATEWAY,\
+T_SRC, T_DEST, T_PKGSIZE, T_REPORTDIR, T_PATH, T_GATEWAY,\
 console_write, string_write
 from inuithy.storage.storage import Storage
 from inuithy.util.config_manager import create_inuithy_cfg, create_traffic_cfg
@@ -31,7 +31,7 @@ class PandasPlugin(object):
         for k, v in rec.items():
             line.append(v)
         ('panid', 'channel', 'msg', 'msgtype', 'gateway', 'genid', 'time', 'traffic_type', 'node', 'clientid', 'host')
-        header = (T_NODE, T_SENDER, T_RECIPIENT, T_TRAFFIC_TYPE, T_CLIENTID, T_HOST, T_GENID, T_TIME)
+        header = (T_NODE, T_SRC, T_DEST, T_TRAFFIC_TYPE, T_CLIENTID, T_HOST, T_GENID, T_TIME)
                 00 01 02 03 04
         node0: [2, 5, 1, 5, 6]
         node1: [2, 5, 1, 5, 6]
@@ -57,14 +57,14 @@ class PandasPlugin(object):
         return repo
 
     @staticmethod
-    def gen_sender_pack(recs, genid, pdf_pg):
+    def gen_src_pack(recs, genid, pdf_pg):
 
         data = {}
         index = set()
 
         try:
             for rec in recs:
-    #            if rec.get(T_MSG_TYPE) != MessageType.SENT.name or rec.get(T_SENDER) is None or rec.get(T_RECIPIENT) is None:
+    #            if rec.get(T_MSG_TYPE) != MessageType.SENT.name or rec.get(T_SRC) is None or rec.get(T_DEST) is None:
                 if rec.get(T_MSG_TYPE) != MessageType.SENT.name:
                     continue
                 n, t = rec.get(T_NODE), rec.get(T_TIME)
@@ -85,14 +85,14 @@ class PandasPlugin(object):
             console_write("ERROR: {}", ex)
 
     @staticmethod
-    def gen_recipient_pack(recs, genid, pdf_pg):
+    def gen_dest_pack(recs, genid, pdf_pg):
         """
         """
         data = {}
         index = set()
         try:
             for rec in recs:
-#            if rec.get(T_MSG_TYPE) != MessageType.RECV.name or rec.get(T_SENDER) is None or rec.get(T_RECIPIENT) is None:
+#            if rec.get(T_MSG_TYPE) != MessageType.RECV.name or rec.get(T_SRC) is None or rec.get(T_DEST) is None:
 #                continue
                 if rec.get(T_MSG_TYPE) != MessageType.RECV.name:
                     continue
@@ -166,7 +166,7 @@ class PandasPlugin(object):
 
         try:
             for rec in recs:
-                if rec.get(T_SENDER) is None or rec.get(T_RECIPIENT) is None:
+                if rec.get(T_SRC) is None or rec.get(T_DEST) is None:
                     continue
                 line = [rec.get(k) for k in header]
                 line_fmt = ('{},' * len(line)).strip(',')
@@ -204,7 +204,7 @@ class PandasPlugin(object):
 #                for r in recs:
 #                    fd.write(str(r)+'\n')
 
-            header = (T_NODE, T_SENDER, T_RECIPIENT, T_TIME, T_MSG_TYPE, T_TRAFFIC_TYPE, T_CLIENTID, T_HOST, T_GENID)
+            header = (T_NODE, T_SRC, T_DEST, T_TIME, T_MSG_TYPE, T_TRAFFIC_TYPE, T_CLIENTID, T_HOST, T_GENID)
             csv_data = PandasPlugin.create_csv(recs, header, genid)
             with open(string_write('{}/{}.csv', cfg.config[T_REPORTDIR][T_PATH], genid), 'w') as fd:
                 fd.write(','.join(h for h in header) + '\n')
@@ -218,8 +218,8 @@ class PandasPlugin(object):
             with PdfPages(string_write('{}/{}.pdf', cfg.config[T_REPORTDIR][T_PATH], genid)) as pdf_pg:
                 [PandasPlugin.groupby(pdata, item, pdf_pg) for item in header] 
                 PandasPlugin.gen_total_pack(recs, genid, pdf_pg) 
-                PandasPlugin.gen_sender_pack(recs, genid, pdf_pg) 
-                PandasPlugin.gen_recipient_pack(recs, genid, pdf_pg) 
+                PandasPlugin.gen_src_pack(recs, genid, pdf_pg) 
+                PandasPlugin.gen_dest_pack(recs, genid, pdf_pg) 
                 PandasPlugin.gen_total_gwpack(recs, genid, pdf_pg)
 
 
