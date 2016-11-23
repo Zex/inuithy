c570a205f51e5f559c08f731fff5b94defe2182d
Zex Li <top_zlynch@yahoo.com>
2016-11-23 13:25:15 +0800

    define proto-related keywords

--- inuithy/common/node.py
+++ inuithy/common/node.py
@@ -1,26 +1,20 @@
 """ General node definition
  @author: Zex Li <top_zlynch@yahoo.com>
 """
-from inuithy.common.predef import TrafficType, T_TRAFFIC_TYPE, T_MSG,\
-INUITHY_LOGCONFIG, T_MSG_TYPE, MessageType, string_write, T_TYPE, T_SPANID,\
-T_ADDR, T_PORT, T_PANID, T_TIME, T_NODE, T_SRC, T_DEST, T_ACK, T_CHANNEL,\
-T_ZBEE_NWK_SRC, T_ZBEE_NWK_DST, T_ZBEE_NWK_ADDR, T_AVGMACRETRY, T_RSP,\
-T_LASTMSGLQI, T_LASTMSGRSSI, T_PKGBUFALLOCFAIL, T_RTDISCINIT, T_YES,\
-T_APSRXBCAST, T_APSTXBCAST, T_APSTXUCASTRETRY, T_RELAYEDUCAST, T_STATUS,\
-T_APSRXUCAST, T_NEIGHBORADDED, T_NEIGHBORRMED, T_NEIGHBORSTALE, T_GENID,\
-T_MACRXUCAST, T_MACTXUCAST, T_MACTXUCASTFAIL, T_MACTXUCASTRETRY,\
-T_MACRXBCAST, T_MACTXBCAST, T_APSTXUCASTSUCCESS, T_APSTXUCASTFAIL,\
-T_UNKNOWN_RESP, T_PKGSIZE, T_ZBEE_ZCL_CMD_TSN, T_SND_SEQ_NR
+from inuithy.common.predef import TrafficType, T_MSG,\
+INUITHY_LOGCONFIG, string_write, T_TYPE, T_ADDR, T_PORT
 from inuithy.util.cmd_helper import pub_reportwrite, pub_notification
-from inuithy.protocol.ble_protocol import BleProtocol as BleProt
-from inuithy.protocol.zigbee_protocol import ZigbeeProtocol as ZbeeProt
+from inuithy.protocol.ble_proto import BleProtocol as BleProt
+from inuithy.protocol.zigbee_proto import ZigbeeProtocol as ZbeeProt
+from inuithy.protocol.zigbee_proto import T_ACK
+from inuithy.protocol.bzcombo_proto import BzProtocol as BZProt
+
 import logging.config as lconf
 from enum import Enum
 import threading
 import logging
 import serial
 import json
-import time
 import random
 
 lconf.fileConfig(INUITHY_LOGCONFIG)
@@ -42,12 +36,19 @@ class SerialNode(object):
         self.addr = None
         self.ntype = ntype
         self.reporter = reporter
+        self.prot = None
         # TODO: create serial object
         self.__serial = None #serial.Serial(port, baudrate=baudrate, timeout=timeout)
         self.__listener = threading.Thread(target=self.__listener_routine, name="Listener")
         self.run_listener = False
         self.genid = None
         self.done = threading.Event()
+
+    def __str__(self):
+        return json.dumps({T_TYPE:self.ntype.name})
+
+    def __del__(self):
+        self.stop_listener()
 
     def read(self, rdbyte=0):
         """Read data ultility"""
@@ -91,20 +92,6 @@ class SerialNode(object):
         """Stop running listener"""
         self.run_listener = False
 
-    def report_write(self, data=None, request=None):
-        """Report written data"""
-        pass
-
-    def report_read(self, data=None):
-        """Report read data"""
-        pass
-
-    def __str__(self):
-        return json.dumps({T_TYPE:self.ntype.name})
-
-    def __del__(self):
-        self.stop_listener()
-
     def join(self, data):
         """General join adapter"""
         pass
@@ -112,6 +99,28 @@ class SerialNode(object):
     def traffic(self, data):
         """General traffic adapter"""
         pass
+
+    def report_read(self, data=None):
+        """"Report received data"""
+        if self.reporter is None or self.genid is None:
+            return
+#TODO: uncomment
+#       if data is None or len(data) == 0:
+#           return
+        report = self.prot.parse_rbuf(data, self)
+        if report is not None and len(report) > 2:
+            pub_notification(self.reporter, data=report)
+
+    def report_write(self, data=None, request=None):
+        """"Report writen data"""
+        if self.reporter is None or self.genid is None:
+            return
+#TODO: uncomment
+#       if data is None or len(data) == 0:
+#           return
+        report = self.prot.parse_wbuf(data, self, request)
+        if report is not None and len(report) > 2:
+            pub_reportwrite(self.reporter, data=report)
 
 class NodeBLE(SerialNode):
     """Bluetooth node definition
@@ -123,6 +132,7 @@ class NodeBLE(SerialNode):
         self.addr = addr
         self.port = port
         self.prot = BleProt
+        self.prot.lgr = self.lgr
 #        if port is not None and len(port) > 0:
 #            self.start_listener()
 
@@ -132,73 +142,29 @@ class NodeBLE(SerialNode):
             T_ADDR:self.addr,
             T_PORT:self.port})
 
-    def report_write(self, data=None, request=None):
-        """"Report writen data"""
-        if self.reporter is None or self.genid is None:
-            return
-#       if data is None or len(data) == 0:
-#           return
-        report = {
-            T_TIME: str(time.time()),
-            T_GENID: self.genid,
-            T_MSG_TYPE: MessageType.SEND.name,
-            T_NODE: self.addr,
-            T_MSG: data,
-            T_SRC: request.get(T_SRC),
-            T_DEST: request.get(T_DEST),
-        }
-        pub_reportwrite(self.reporter, data=report)
-
-    def report_read(self, data=None):
-        """"Report received data"""
-        if self.reporter is None or self.genid is None:
-            return
-#       if data is None or len(data) == 0:
-#           return
-        #TODO parse data
-        report = {
-            T_TIME: str(time.time()),
-            T_GENID: self.genid,
-            T_MSG_TYPE: MessageType.RECV.name,
-            T_NODE: self.addr,
-            T_MSG: data,
-            T_SRC: 'SOMEONE', #TODO
-            T_DEST: self.addr,
-        }
-#        if data.split(' ')[0] == 'joingrp':
-#            report[T_TRAFFIC_TYPE] = TrafficType.JOIN.name
-#        else
-#            report[T_TRAFFIC_TYPE] = TrafficType.SCMD.name
-        msg_type = random.randint(TrafficType.JOIN.value, TrafficType.SCMD.value)
-        report[T_TRAFFIC_TYPE] = msg_type == TrafficType.JOIN.value\
-            and TrafficType.JOIN.name or TrafficType.SCMD.name
-
-        report.__setitem__(T_MSG_TYPE, MessageType.RECV.name)
-        pub_notification(self.reporter, data=report)
-
     def join(self, data):
         """General join request adapter"""
         self.genid = data.get(T_GENID)
-        self.joingrp(data.get(T_PANID), request=data)
+        self.joingrp(data)
 
     def traffic(self, data):
         """General traffic request adapter"""
         self.genid = data.get(T_GENID)
-        self.lighton(data.get(T_DEST), request=data)
+        self.lighton(data)
 
-    def joingrp(self, grpid, request=None):
+    def joingrp(self, request):
         """Send joingrp command"""
-        msg = self.prot.joingrp(grpid)
+        msg = self.prot.joingrp(request)
         self.write(msg, request)
 
-    def leavegrp(self, grpid, request=None):
+    def leavegrp(self, request):
         """Send leavegrp command"""
-        msg = self.prot.leavegrp(grpid)
+        msg = self.prot.leavegrp(request)
         self.write(msg, request)
 
-    def lighton(self, raddr, request=None):
+    def lighton(self, request):
         """Send lighton command"""
-        msg = self.prot.lighton(raddr)
+        msg = self.prot.lighton(request)
         self.write(msg, request)
 
     def lightoff(self, raddr, request=None):
@@ -226,9 +192,10 @@ class NodeZigbee(SerialNode):
         self.addr = addr
         self.uid = None
         self.port = port
-        self.prot = ZbeeProt()
         self.sequence_nr = 0
         self.joined = False # DEBUG data
+        self.prot = ZbeeProt
+        self.prot.lgr = self.lgr
 
     def __str__(self):
         return json.dumps({
@@ -239,171 +206,23 @@ class NodeZigbee(SerialNode):
     def join(self, data):
         """General join request adapter"""
         self.genid = data.get(T_GENID)
-        self.joinnw(data.get(T_CHANNEL), data.get(T_PANID), data.get(T_SPANID), data.get(T_NODE), request=data)
+        self.joinnw(data)
 
     def traffic(self, data):
         """General traffic request adapter"""
         self.genid = data.get(T_GENID)
-        self.writeattribute2(data.get(T_DEST), data.get(T_PKGSIZE), 1, request=data)
+        data[T_RSP] = 1
+        self.writeattribute2(data)
 
-    def joinnw(self, ch, ext_panid, panid, addr, request=None):
+    def joinnw(self, request):
         """Send join command"""
-        msg = self.prot.joinnw(ch, ext_panid, panid, addr)
+        msg = self.prot.joinnw(request)
         self.write(msg, request)
 
-    def writeattribute2(self, dest, psize, rsp, request=None):
+    def writeattribute2(self, request):
         """Send writeattribute2 command"""
-        msg = self.prot.writeattribute2(dest, psize, rsp)
-        request[T_RSP] = rsp
+        msg = self.prot.writeattribute2(request)
         self.write(msg, request)
-
-    def report_write(self, data=None, request=None):
-        """"Report writen data"""
-        if self.reporter is None or self.genid is None:
-            return
-#       if data is None or len(data) == 0:
-#           return
-        report = {
-            T_TIME: time.time(),
-            T_GENID: self.genid,
-            T_MSG_TYPE: MessageType.SEND.name,
-            T_NODE: self.addr,
-            T_TYPE: self.prot.ReqType.snd_req.name,
-            T_ZBEE_NWK_SRC: request.get(T_SRC),#self.addr,
-            T_ZBEE_NWK_DST: request.get(T_DEST),
-            T_ACK: request.get(T_RSP) == 1 and 'y' or 'n',
-        }
-        pub_reportwrite(self.reporter, data=report)
-
-    def report_read(self, data=None):
-        """"Report received data"""
-        if self.reporter is None or self.genid is None:
-            return
-#       if data is None or len(data) == 0:
-#           return
-        report = {}
-        params = data.split(' ')
-
-        # DEBUG data
-        rand = random.randint(MessageType.RECV.value, MessageType.UNKNOWN.value)
-        if self.joined is False:
-#        if rand == MessageType.JOINING.value
-            params = []
-            params.append(MessageType.JOINING.name)
-        else:
-            if rand == MessageType.RECV.value:
-                params = []
-                params.append(MessageType.RECV.name)
-                params.append(str(random.randint(0, 10)))
-                params.append(str(random.randint(0, 10)))
-                params.append(str(random.randint(1100, 1144)))
-            elif rand == MessageType.SEND.value:
-                params = []
-                params.append(MessageType.SEND.name)
-                params.append(str(random.randint(0, 10)))
-                params.append(str(random.randint(0, 10)))
-                params.append(str(random.randint(0, 10)))
-                params.append(str(random.randint(1100, 1144)))
-                params.append(str(random.randint(10, 100)))
-            else:
-                params = [self.prot.DGN]
-                params.extend([str(random.randint(0, 2000)) for _ in range(21)])
-        #------------end debug data--------------
-        msg_type = params[0].upper()
-
-        if msg_type == MessageType.SEND.name:
-            if len(params) == 6:
-                report = {\
-                    T_TIME: time.time(),\
-                    T_GENID: self.genid,\
-                    T_TRAFFIC_TYPE: TrafficType.SCMD.name,\
-                    T_NODE: self.addr,
-                    T_MSG_TYPE: MessageType.RECV.name,
-                    T_ZBEE_NWK_SRC: self.addr,\
-                    T_ZBEE_NWK_DST: params[4],\
-                    T_TYPE: self.prot.MsgType.snd.name,\
-                    T_ZBEE_ZCL_CMD_TSN: params[1],\
-                    T_STATUS: params[5],\
-                    T_SND_SEQ_NR: self.sequence_nr,\
-                }
-                self.sequence_nr += 1
-#                if status == '0x00':
-#                    self.nr_messages_sent += 1
-            else:
-                self.lgr.error(string_write('Incorrect send confirm: {}', data))
-        elif msg_type == MessageType.RECV.name:
-            report = {\
-                    T_TIME: time.time(),\
-                    T_GENID: self.genid,\
-                    T_TRAFFIC_TYPE: TrafficType.SCMD.name,\
-                    T_MSG_TYPE: MessageType.RECV.name,
-                    T_NODE: self.addr,
-                    T_TYPE: self.prot.MsgType.rcv.name,\
-                    T_ZBEE_NWK_SRC: params[3],\
-                    T_ZBEE_NWK_DST: self.addr,\
-                    T_ZBEE_ZCL_CMD_TSN: params[1],\
-            }
-        elif msg_type == MessageType.JOINING.name:
-            report = {\
-                T_TIME: time.time(),\
-                T_GENID: self.genid,\
-                T_TRAFFIC_TYPE: TrafficType.JOIN.name,\
-                T_MSG_TYPE: MessageType.RECV.name,\
-                T_NODE: self.addr,\
-            }
-            #DEBUG data
-            self.joined = True
-        elif params[0] == 'Trying':
-            pass
-        elif params[0] == 'Network':
-            pass
-        elif params[0] == self.prot.GETUID:
-            self.uid = params[1]
-        elif params[0] == self.prot.GETNETWORKADDRESS:
-            self.addr = data[-6:]
-        elif params[0] == self.prot.RESET_CONF:
-            pass
-        elif params[0] == self.prot.DGN:
-            report = {
-                T_TIME: time.time(),\
-                T_GENID: self.genid,\
-                T_TRAFFIC_TYPE: TrafficType.SCMD.name,\
-                T_MSG_TYPE: MessageType.RECV.name,\
-                T_NODE: self.addr,\
-                T_TYPE: self.prot.MsgType.dgn.name,
-                T_ZBEE_NWK_ADDR: self.addr,\
-                T_AVGMACRETRY: params[1],\
-                T_LASTMSGLQI: params[2],\
-                T_LASTMSGRSSI: params[3],\
-                T_PKGBUFALLOCFAIL: params[4],\
-                T_RTDISCINIT: params[5],\
-                T_APSRXBCAST: params[6],\
-                T_APSTXBCAST: params[7],\
-                T_APSTXUCASTRETRY: params[8],\
-                T_RELAYEDUCAST: params[9],\
-                T_APSRXUCAST: params[10],\
-                T_NEIGHBORADDED: params[11],\
-                T_NEIGHBORRMED: params[12],\
-                T_NEIGHBORSTALE: params[13],\
-                T_MACRXUCAST: params[14],\
-                T_MACTXUCAST: params[15],\
-                T_MACTXUCASTFAIL: params[16],\
-                T_MACTXUCASTRETRY: params[17],\
-                T_MACRXBCAST: params[18],\
-                T_MACTXBCAST: params[19],\
-                T_APSTXUCASTSUCCESS: params[20],\
-                T_APSTXUCASTFAIL: params[21],\
-            }
-        else:
-            report = {\
-                T_TIME: time.time(),\
-                T_GENID: self.genid,\
-                T_MSG_TYPE: MessageType.RECV.name,\
-                T_NODE: self.addr,\
-                T_UNKNOWN_RESP: T_YES,\
-                T_ZBEE_NWK_ADDR: ' '.join([self.addr, data]),\
-            }
-        pub_notification(self.reporter, data=report)
 
 if __name__ == '__main__':
     lgr = logging.getLogger('InuithyNode')
