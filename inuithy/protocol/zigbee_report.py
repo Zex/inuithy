329978df0ee2b1bcd078990de352b906b83975bf
Zex Li <top_zlynch@yahoo.com>
2016-11-25 13:26:23 +0800

    create dataframe from mongodb records

--- inuithy/protocol/zigbee_report.py
+++ inuithy/protocol/zigbee_report.py
@@ -24,10 +24,11 @@ import logging.config as lconf
 import json
 import sys
 from os import path, mkdir
+from copy import deepcopy
 
 mplib.style.use('ggplot')
-#lconf.fileConfig(INUITHY_LOGCONFIG)
-#lgr = logging
+lconf.fileConfig(INUITHY_LOGCONFIG)
+lgr = logging
 
 class ZbeeReport(object):
     """Zigbee report helper"""
@@ -35,29 +36,38 @@ class ZbeeReport(object):
     def create_csv(recs, ginfo):
         """Create CSV format from records
         """
-        data = []
-        index = set()
-
-        try:
-            for rec in recs:
-                if len([rec.get(k) for k in ginfo.header if rec.get(k) is not None]) != len(header):
-                    continue
-                line = [rec.get(k) for k in ginfo.header if rec.get(k)]
-                line_fmt = ('{},' * len(line)).strip(',')
-                data.append(string_write(line_fmt, *tuple(line)))
-        except Exception as ex:
-            console_write("ERROR: {}", ex)
+        lgr.info(string_write("Create csv data for genid {}", ginfo.genid))
+        df = pd.DataFrame.from_records(recs)
+        df = df.fillna(value=0)
+        data = df.to_csv(columns=ginfo.header, index=False)
         return data
+
+#        data = []
+#        index = set()
+#
+#        try:
+#            for rec in recs:
+#                if len([rec.get(k) for k in ginfo.header if rec.get(k) is not None]) != len(ginfo.header):
+#                    continue
+#                line = [rec.get(k) for k in ginfo.header if rec.get(k)]
+#                print(line)
+#                line_fmt = ('{},' * len(line)).strip(',')
+#                data.append(string_write(line_fmt, *tuple(line)))
+#        except Exception as ex:
+#            lgr.error("Exception on CSV creation: {}", ex)
+#            raise
+#        return data
 
     @staticmethod
     def item_based(pdata, item, pdf_pg, nodes=None, iloc_range=None, title=None, fig_base=None):
+        lgr.info(string_write("Creating figure for item {}", item))
         try:
             df = None
-            if nodes is None:
+            if nodes is None or len(nodes) == 0:
                 addr_grp = pdata.groupby([T_ZBEE_NWK_ADDR], as_index=False)
                 nodes = addr_grp.groups.keys()
 
-            xindex = pdata[T_TIME].diff()
+            xindex = pdata[T_TIME]#.diff()
 
             for addr in nodes:
                 ser = pdata[item][pdata[T_ZBEE_NWK_ADDR] == addr].diff()
@@ -79,9 +89,10 @@ class ZbeeReport(object):
                 if pdf_pg is not None:
                     pdf_pg.savefig()
             else:
-                console_write("WARN: DataFrame is empty")
+                lgr.warning("WARN: DataFrame is empty")
         except Exception as ex:
-            console_write("ERROR: {}", ex)
+            lgr.error(string_write("Exception on creating item based figure: {}", ex))
+            raise
 
     @staticmethod
     def prep_info(genid, inuithy_cfgpath=INUITHY_CONFIG_PATH):
@@ -92,6 +103,7 @@ class ZbeeReport(object):
         routeDiscInitiated,relayedUcast,packetBufferAllocateFailure,
         apsTxBcast,apsTxUcastSuccess,apsTxUcastFail,apsTxUcastRetry,apsRxBcast,apsRxUcast
         """
+        lgr.info(string_write("Prepare generation info with {}", genid))
         ginfo = GenInfo()
 
         ginfo.cfg = create_inuithy_cfg(inuithy_cfgpath)
@@ -107,11 +119,11 @@ class ZbeeReport(object):
         ginfo.pdf_path = string_write('{}/{}.pdf',\
             ginfo.cfg.config[T_REPORTDIR][T_PATH], ginfo.genid)
 
-        ginfo.header = (T_TIME, T_ZBEE_NWK_ADDR,\
+        ginfo.header = [T_TIME, T_ZBEE_NWK_ADDR,\
             T_MACTXBCAST, T_MACTXUCASTRETRY, T_MACTXUCASTFAIL, T_MACTXUCAST, T_MACRXBCAST,\
             T_MACRXUCAST, T_NEIGHBORADDED, T_NEIGHBORRMED, T_NEIGHBORSTALE, T_AVGMACRETRY,\
             T_RTDISCINIT, T_RELAYEDUCAST, T_PKGBUFALLOCFAIL, T_APSTXBCAST, T_APSTXUCASTSUCCESS,\
-            T_APSTXUCASTFAIL, T_APSTXUCASTRETRY, T_APSRXBCAST, T_APSRXUCAST)
+            T_APSTXUCASTFAIL, T_APSTXUCASTRETRY, T_APSRXBCAST, T_APSRXUCAST]
         if not path.isdir(ginfo.fig_base):
             mkdir(ginfo.fig_base)
         return ginfo
@@ -125,57 +137,75 @@ class ZbeeReport(object):
         routeDiscInitiated,relayedUcast,packetBufferAllocateFailure,
         apsTxBcast,apsTxUcastSuccess,apsTxUcastFail,apsTxUcastRetry,apsRxBcast,apsRxUcast
         """
-        storage = Storage(cfg, lgr)
+        lgr.info(string_write("Generate csv with ginfo[{}]", ginfo))
+        storage = Storage(ginfo.cfg, lgr)
+
         for r in storage.trafrec.find({
                 T_GENID: ginfo.genid,
             }):
             recs = r.get(T_RECORDS)
-            csv_data = ZbeeReport.create_csv(recs, ginfo.header, ginfo.genid)
-
-            with open(ginfo.csv_path, 'w') as fd:
-                fd.write(','.join(h for h in ginfo.header) + '\n')
-                [fd.write(line + '\n') for line in csv_data]
+            #csv_data = ZbeeReport.create_csv(recs, ginfo)
+            pdata = pd.DataFrame.from_records(recs)
+            pdata = pdata[pd.notnull(pdata[ginfo.header[2]])]
+#            print(pdata)
+#            pdata = pdata.fillna(value=0)
+            [pdata.__setitem__(item, pdata[item].astype(int)) for item in ginfo.header[2:]]
+            pdata.to_csv(ginfo.csv_path, columns=ginfo.header, index=False)
+            return pdata
+            
+#            with open(ginfo.csv_path, 'w') as fd:
+#                fd.write(','.join(h for h in ginfo.header) + '\n')
+#                [fd.write(line + '\n') for line in csv_data]
 
     @staticmethod
-    def gen_report(ginfo, gw=None, interet_nodes=None, irange=None):
+    def gen_report(pdata, ginfo, gw=None, interest_nodes=None, irange=None):
         """Report generation helper"""
-        pdata = pd.read_csv(ginfo.csv_path, index_col=False, names=list(ginfo.header), header=None)
-        console_write(pdata.info())
-#       pdata[T_TIME] = pdata[T_TIME].astype(str)
+        lgr.info(string_write("Generate report with gw=[{}], nodes=[{}] irange={}", gw, interest_nodes, irange))
+#        pdata = pd.read_csv(ginfo.csv_path, index_col=False, names=ginfo.header, header=None)
+        pdata.info(verbose=True)
+#        pdata[T_TIME] = pdata[T_TIME].astype()
         pdata.index = pdata[T_TIME]#.diff()
         with PdfPages(ginfo.pdf_path) as pdf_pg:
+            if gw is not None and len(gw) > 0:
+                for gwnode in gw: # Each subnet
+                    for item in ginfo.header[2:]:
+                        ZbeeReport.item_based(pdata, item, pdf_pg, gwnode,\
+                        title = string_write("{} by gateway", item), fig_base=ginfo.fig_base)
             for item in ginfo.header[2:]:
-                ZbeeReport.item_based(pdata, item, pdf_pg, [gw],\
-                    title = string_write("{} by gateway", item), fig_base=ginfo.fig_base)
-            for item in ginfo.header[2:]:
-                ZbeeReport.item_based(pdata, item, pdf_pg, nodes, irange, fig_base=ginfo.fig_base)
+                ZbeeReport.item_based(pdata, item, pdf_pg, interest_nodes, irange, fig_base=ginfo.fig_base)
+
+    @staticmethod
+    def generate(genid, gw=None, nodes=None, irange=None, cfgpath=INUITHY_CONFIG_PATH):
+        """Generate CSV data and traffic analysis figures"""
+        try:
+            ginfo = ZbeeReport.prep_info(genid, cfgpath)
+            """
+            nodes = ['0x0000', '0x0001', '0x0102', '0x0103',\
+                     '0x0205', '0x0206', '0x0303', '0x0304',\
+                     '0x0400', '0x0401']
+            gw = '0x0207'
+            irange = None #(70, 150)
+            """
+            """
+            nodes = None
+            gw = '0x0303'
+            irange = None
+            """
+#TODO: uncomment
+            pdata = ZbeeReport.gen_csv(ginfo)
+#           ginfo.csv_path = 'docs/UID1478067701.csv'
+#           ginfo.csv_path = 'docs/UID1470021754.csv'
+            ZbeeReport.gen_report(pdata, ginfo, gw, nodes, irange)
+        except Exception as ex:
+            lgr.error(string_write("Exception on generate reports: {}", ex))
 
 if __name__ == '__main__':
 
 #    ZbeeReport.gen_report(genid='581fdfe3362ac719d1c96eb3')
 #    ZbeeReport.gen_report(genid='1478508817')
 #    ZbeeReport.gen_report(genid='1478585096')
-
     if len(sys.argv) > 1:
-        ginfo = ZbeeReport.prep_info(sys.argv[1])
-        nodes, gw, irange = None, None, None
-        """
-        nodes = ['0x0000', '0x0001', '0x0102', '0x0103',\
-                 '0x0205', '0x0206', '0x0303', '0x0304',\
-                 '0x0400', '0x0401']
-        gw = '0x0207'
-        irange = None #(70, 150)
-        """
-        """
-        nodes = None
-        gw = '0x0303'
-        irange = None
-        """
-#TODO: uncomment
-        ZbeeReport.gen_csv(genid=sys.argv[1], ginfo)
-#       ginfo.csv_path = 'docs/UID1478067701.csv'
-#       ginfo.csv_path = 'docs/UID1470021754.csv'
-        ZbeeReport.gen_report(ginfo, gw, nodes, irange)
+        ZbeeReport.generate(sys.argv[1], gw=None, nodes=None, irange=None)
     else:
         console_write("Genid not given")
 
