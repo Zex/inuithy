83979116e117bf113ddfca4fe2f0dd07dd6ab3af
Zex Li <top_zlynch@yahoo.com>
2016-12-02 17:05:00 +0800

    unify supported protocol

--- inuithy/common/agent_info.py
+++ inuithy/common/agent_info.py
@@ -2,11 +2,12 @@
  @author: Zex Li <top_zlynch@yahoo.com>
 """
 from inuithy.common.predef import T_TYPE, AgentStatus, T_ADDR, to_string
-from inuithy.common.node import NodeBLE, NodeZigbee, NodeType
+from inuithy.common.supported_proto import SupportedProto
 import json
 
-class AgentInfo(object):
-    def __init__(self, agentid ="", host ="", status =AgentStatus.OFFLINE, nodes =[]):
+class AgentInfo(SupportedProto):
+    """Agent information block"""
+    def __init__(self, agentid="", host="", status=AgentStatus.OFFLINE, nodes=None):
         self.agentid = agentid
         self.status = status
         self.host = host
@@ -16,14 +17,17 @@ class AgentInfo(object):
     def rebuild_node(self, nodes):
         """Rebuild nodes from node infomation
         """
+        if nodes is None:
+            return
         for n in nodes:
+            node = None
             n = json.loads(n)
-            if n[T_TYPE] == NodeType.BLE.name:
-                self.nodes.append(NodeBLE(addr =n[T_ADDR]))
-            elif n[T_TYPE] == NodeType.Zigbee.name:
-                self.nodes.append(NodeZigbee(addr =n[T_ADDR]))
-            else:
-                pass
+            proto = SupportedProto.protocols.get(T_TYPE)
+            if proto is not None:
+                node = proto[1].create(addr=n.get(T_ADDR))
+            
+            if node is not None:
+                self.nodes.append(proto[1].create(addr=n.get(T_ADDR)))
 
     def has_node(self, addr):
         for node in self.nodes:
@@ -34,3 +38,7 @@ class AgentInfo(object):
         return to_string("agent<{}>: host:{} status:{} nodes:{} total_node:{}",\
             self.agentid, self.host, self.status, [str(n) for n in self.nodes], len(self.nodes))
     
+if __name__ == '__main__':
+    ai = AgentInfo('123', 'lkfjrj', nodes=[json.dumps(6)])
+    print(ai)
+
--- inuithy/common/serial_adapter.py
+++ inuithy/common/serial_adapter.py
@@ -2,18 +2,18 @@
  @author: Zex Li <top_zlynch@yahoo.com>
 """
 from inuithy.common.predef import DEV_TTYUSB, DEV_TTYS, DEV_TTY,\
-to_string, T_EVERYONE, INUITHY_LOGCONFIG
-from inuithy.common.node import NodeBLE, NodeZigbee, NodeType
-from inuithy.protocol.ble_proto import BleProtocol as BleProt
-from inuithy.protocol.zigbee_proto import ZigbeeProtocol as ZbeeProt
-from inuithy.protocol.bzcombo_proto import BzProtocol as BZProt
+to_string, T_EVERYONE, INUITHY_LOGCONFIG, T_MSG
+from inuithy.common.supported_proto import SupportedProto
+from inuithy.protocol.ble_proto import BleProtocol as BleProto
+from inuithy.protocol.zigbee_proto import ZigbeeProtocol as ZbeeProto
+from inuithy.protocol.bzcombo_proto import BzProtocol as BZProto
 from inuithy.util.task_manager import ThrTaskManager
 import glob, logging
 import logging.config as lconf
 
 lconf.fileConfig(INUITHY_LOGCONFIG)
 
-class SerialAdapter(object):
+class SerialAdapter(SupportedProto):
     """Serial port adapter
     """
     @property
@@ -26,13 +26,18 @@ class SerialAdapter(object):
     def __init__(self, reporter=None, lgr=None):
         self.__nodes = []
         self.reporter = reporter
-        self.lgr = lgr
-        if self.lgr is None:
-            self.lgr = logging
+        SerialAdapter.lgr = lgr
+        if SerialAdapter.lgr is None:
+            SerialAdapter.lgr = logging
 
     @staticmethod
     def exam_msg(msg):
         """Send examine message"""
+# TODO
+#        return ZbeeProto.NAME
+        return BleProto.NAME
+#        return BZProto.NAME
+
         buf = ''
         dev.write(msg)
 
@@ -40,32 +45,29 @@ class SerialAdapter(object):
             buf = dev.readall()
         if len(buf) != 0:
             buf = buf.strip('\t \r\n')
-# TODO Examine reply
-#        return NodeType.BLE
-#        return NodeType.Zigbee
-#        return NodeType.BleZbee
-        return None
+        return buf
 
     @staticmethod
     def get_type(port):
         """Get firmware type via port"""
-        # TODO
-#        return NodeType.BLE
-        return NodeType.Zigbee
-#        return NodeType.BleZbee
-
-        supported_proto = [BleProt, ZbeeProt, BZProt]
-        for msg in supported_proto:
+        for proto in SerialAdapter.protocols.values():
+            """name => (proto, node)"""
             try:
-                ptype = SerialAdapter.exam_msg(msg)
-                if ptype is not None and len(ptype) > 0:
-                    return ptype
+                req = proto[0].getfwver()
+                rep = SerialAdapter.exam_msg(req)
+                if rep is not None and len(rep) > 0:
+                    SerialAdapter.lgr.info(to_string("Reply {}", rep))
+                    if proto[0].isme({T_MSG: rep}):
+                        SerialAdapter.lgr.info(to_string("Found protocol {}", proto[0]))
+                        return proto[1]
             except Exception as ex:
-                self.lgr(to_string(
-                    "Exception on sending examine msg [{}]: {}",
-                    msg, ex))
+                SerialAdapter.lgr.error(to_string(
+                "Exception on sending examine msg [{}]: {}", req, ex))
+        return None
 
     def create_node(self, port):
+        """Create node"""
+        SerialAdapter.lgr.info(to_string("Creating node {}", port))
         if isinstance(port, tuple):
             port = port[1]
         if not isinstance(port, str) or port is None or len(port) == 0:
@@ -74,37 +76,36 @@ class SerialAdapter(object):
         node = None
         try:
             ptype = SerialAdapter.get_type(port)
-            if ptype is NodeType.BLE:
-                node = NodeBLE(port, reporter=self.reporter, lgr=self.lgr)
-            elif ptype is NodeType.Zigbee:
-                node = NodeZigbee(port, reporter=self.reporter, lgr=self.lgr)
-            elif ptype is NodeType.BleZbee:
-                node = NodeBz(port, reporter=self.reporter, lgr=self.lgr)
+            if ptype is not None:
+                node = ptype.create(port, reporter=self.reporter, lgr=SerialAdapter.lgr)
+            else: 
+                SerialAdapter.lgr.error(to_string("Unsupported protocol {}", ptype))
             if node is not None:
                 self.__nodes.append(node)
                 node.start_listener()
         except Exception as ex:
-            logging.error(to_string("Exception on creating node: {}", ex))
-
+            SerialAdapter.lgr.error(to_string("Exception on creating node: {}", ex))
+#        SerialAdapter.lgr.debug("Creation finished " + port)
         return node
 
     def scan_nodes(self, targets=DEV_TTYUSB.format(T_EVERYONE)):
-        self.lgr.info("Scan for connected nodes")
+        SerialAdapter.lgr.info(to_string("Scan for connected nodes {}", targets))
         self.__nodes = []
         ports = enumerate(name for name in glob.glob(targets))
-        mng = ThrTaskManager()
+        mng = ThrTaskManager(SerialAdapter.lgr)
         mng.create_task_foreach(self.create_node, ports)
         mng.waitall()
 
     def stop_nodes(self):
-        self.lgr.info("Stop connected nodes")
+        SerialAdapter.lgr.info("Stop connected nodes")
         [n.stop_listener() for n in self.nodes]
 
 if __name__ == '__main__':
 
     sad = SerialAdapter()
-    sad.scan_nodes()
-    print(sad.nodes)
 
+    print(SerialAdapter.protocols)
+    sad.scan_nodes(targets='/dev/ttyS1*')
+    print(sad.nodes)
 
 
