f0f950ee284a01fa5b4533928dd1a96b8a9bafa3
Zex Li <top_zlynch@yahoo.com>
2016-12-12 16:08:01 +0800

    add RawNode

--- inuithy/common/node.py
+++ inuithy/common/node.py
@@ -7,31 +7,35 @@ from inuithy.util.cmd_helper import pub_reportwrite, pub_notification
 from inuithy.protocol.ble_proto import BleProtocol as BleProto
 from inuithy.protocol.zigbee_proto import ZigbeeProtocol as ZbeeProto
 from inuithy.protocol.bzcombo_proto import BzProtocol as BzProto
-#from inuithy.protocol.zigbee_proto import T_RSP
 
 import logging.config as lconf
 import threading
 import logging
 import serial
+import socket
 import json
+import os
+from os.path import dirname, isdir, exists
+from os import makedirs, unlink
 from random import randint
+
+RAWNODE_BASE = '/tmp'
+RAWNODE_SVR = RAWNODE_BASE+'/dev/svr'
+RAWNODE_RBUF_MAX = 1024
 
 lconf.fileConfig(INUITHY_LOGCONFIG)
 
-
-class SerialNode(object):
+class Node(object):
     """Node control via serial port
     """
-    def __init__(self, ntype=None, proto=None, port="", addr="", reporter=None, lgr=None, timeout=2, adapter=None):
-        self.lgr = lgr is None and logging or lgr
-        self.port = port
+    def __init__(self, ntype=None, proto=None, path="", addr="", reporter=None,\
+        lgr=None, adapter=None):
+        self.lgr = logging#lgr is None and logging or lgr
+        self.path = path
         self.addr = addr
         self.ntype = ntype
         self.reporter = reporter
         self.proto = proto
-        # TODO: create serial object
-        self.__serial = None #serial.Serial(port, baudrate=baudrate, timeout=timeout)
-#        self.__listener = threading.Thread(target=self.__listener_routine, name="Listener")
         self.run_listener = False
         self.genid = None
         self.read_event = threading.Event()
@@ -39,6 +43,7 @@ class SerialNode(object):
         self.fwver = ''
         self.adapter = adapter
         self.sequence_nr = 0
+        self.dev = None
 
     def __str__(self):
         if self.ntype is None:
@@ -48,32 +53,21 @@ class SerialNode(object):
         return json.dumps({
             T_TYPE:self.ntype.name,
             T_ADDR:self.addr,
-            T_PORT:self.port})
+            T_PORT:self.path})
 
     def read(self, rdbyte=0):
         """Read data ultility"""
 #        self.lgr.debug(to_string("SerialNode#R: rdbyte:{}", rdbyte))
-        rdbuf = ""
-        if self.__serial is not None and self.__serial.isOpen():
-            if 0 < self.__serial.inWaiting():
-                rdbuf = self.__serial.readall()
-        #TODO -->
-        self.report_read(rdbuf)
-        return rdbuf
+        pass
 
     def write(self, data="", request=None):
         """Write data ultility"""
 #        self.lgr.debug(to_string("SerialNode#W: data:[{}], len:{}", data, len(data)))
-        written = 0
-        if self.__serial is not None and self.__serial.isOpen():
-            written = self.__serial.write(data)
-        #TODO -->
-        self.report_write(data, request)
-        self.read_event.set()
+        pass
 
     def start_listener(self):
         """Start listening incoming package"""
-        if self.run_listener is False and self.port is not None and len(self.port) > 0:
+        if self.run_listener is False and self.path is not None and len(self.path) > 0:
             self.run_listener = True
             self.__listener = threading.Thread(target=self.__listener_routine, name="Listener")
             if self.__listener is not None:
@@ -83,8 +77,8 @@ class SerialNode(object):
         """Listen for incoming packages"""
         while self.run_listener:
             try:
-                if self.__serial is None: #DEBUG
-                    self.read_event.wait()
+                if self.dev is None: #DEBUG
+                    self.read_event.wait()  # triggered by writer
                     self.read_event.clear()
                     self.read_event.wait(randint(1, 3)) #TODO remove
                 self.read()
@@ -98,13 +92,13 @@ class SerialNode(object):
         self.read_event.set()
 
     def join(self, data):
-        """General join adapter"""
+        """General join adapter for joining a node to network"""
         self.genid = data.get(T_GENID)
         msg = self.proto.join(data)
         self.write(msg, data)
 
     def traffic(self, data):
-        """General traffic adapter"""
+        """General traffic adapter for sending network traffic"""
         self.genid = data.get(T_GENID)
         msg = self.proto.traffic(data)
         self.write(msg, data)
@@ -116,8 +110,8 @@ class SerialNode(object):
 #TODO: uncomment
 #       if data is None or len(data) == 0:
 #           return
-#TODO: remove
             if self.proto is None:
+#TODO: use actual data
                 data = ZbeeProto.NAME
 #               data = BleProto.NAME
 #               date = BzProto.NAME
@@ -157,13 +151,124 @@ class SerialNode(object):
         except Exception as ex:
             self.lgr.error(to_string("Exception on report write: {}", ex))
 
+    def close(self):
+
+        if self.dev is not None:
+            self.dev.close()
+            if isinstance(self.dev, socket.socket) and len(self.dev.getsockname()) > 0:
+               os.remove(self.dev.getsockname()) 
+
     @staticmethod
-    def create(ntype=None, proto=None, port='', addr='', reporter=None, lgr=None, adapter=None):
-        """ntype=None, proto=None, port="", addr="", reporter=None, lgr=None, timeout=2, adapter=None
+    def create(ntype=None, proto=None, path='', addr='', repather=None, lgr=None, adapter=None):
+        """ntype=None, proto=None, path="", addr="", repather=None, lgr=None, timeout=2, adapter=None
         """
-        return SerialNode(ntype=ntype, proto=proto, port=port,\
-            addr=addr, reporter=reporter, lgr=lgr, adapter=adapter)
+        try:
+            return Node(ntype=ntype, proto=proto, port=port,\
+                addr=addr, reporter=reporter, lgr=lgr, adapter=adapter)
+        except Exception as ex:
+            lgr.error(to_string("Exception on node creation: {}", ex))
+
+class SerialNode(Node):
+
+    def __init__(self, ntype=None, proto=None, path="", addr="", reporter=None,\
+        lgr=None, timeout=2, baudrate=115200, adapter=None):
+        Node.__init__(self, ntype=ntype, proto=proto, path=path, addr=addr,\
+            reporter=reporter, lgr=lgr, adapter=adapter)
+        self.dev = serial.Serial(port, baudrate=baudrate, timeout=timeout)
+
+    def read(self, rdbyte=0):
+        """Read data ultility"""
+#        self.lgr.debug(to_string("SerialNode#R: rdbyte:{}", rdbyte))
+        rdbuf = ""
+        if isinstance(self.dev, serial.Serial) and self.dev.isOpen():
+            if 0 < self.dev.inWaiting():
+                rdbuf = self.dev.readall()
+                if isinstance(rdbuf, bytes):
+                    rdbuf = rdbuf.decode()
+        #TODO -->
+        self.report_read(rdbuf)
+        return rdbuf
+
+    def write(self, data="", request=None):
+        """Write data ultility"""
+#        self.lgr.debug(to_string("SerialNode#W: data:[{}], len:{}", data, len(data)))
+        written = 0
+        if isinstance(self.dev, serial.Serial) and self.dev.isOpen():
+            if isinstance(data, str):
+                written = self.dev.write(data.encode())
+            else:
+                written = self.dev.write(data)
+        #TODO -->
+        self.report_write(data, request)
+        if self.run_listener:
+            self.read_event.set()
+
+class RawNode(Node):
+    """Raw socket node for simulation"""
+    def __init__(self, ntype=None, proto=None, path="", addr="", reporter=None,\
+        lgr=None, adapter=None):
+        Node.__init__(self, ntype=ntype, proto=proto, path=path, addr=addr,\
+            reporter=reporter, lgr=lgr, adapter=adapter)
+        if not isdir(dirname(path)):
+            makedirs(dirname(path))
+        if exists(path):
+            unlink(path)
+        self.dev = socket.socket(socket.AF_UNIX, socket.SOCK_RAW)
+        self.dev.bind(path)
+
+    def read(self, rdbyte=0):
+        """Read data ultility"""
+        rdbuf = ""
+        rdbuf = self.dev.recv(RAWNODE_RBUF_MAX).decode()
+        print("NODE|R:", rdbuf)
+        self.report_read(rdbuf)
+        return rdbuf
+
+    def write(self, data="", request=None):
+        """Write data ultility"""
+        written = 0
+        if isinstance(self.dev, socket.socket): #DEBUG
+            data = to_string('{}:{}', self.dev.getsockname(), data)
+            print("NODE|W:", data)
+            self.dev.sendto(data.encode(), socket.MSG_DONTWAIT, RAWNODE_SVR)
+        #TODO -->
+        self.report_write(data, request)
+        if self.run_listener:
+            self.read_event.set()
+
+    def close(self):
+        self.dev.close()
+        if len(self.dev.getsockname()) > 0:
+            os.remove(self.dev.getsockname()) 
+
+class RawNodeSvr(RawNode):
+    """Raw node server"""
+    def __init__(self, ntype=None, proto=None, path=RAWNODE_SVR, addr="", reporter=None,\
+        lgr=None, adapter=None):
+        RawNode.__init__(self, ntype=ntype, proto=proto, path=path, addr=addr,\
+            reporter=reporter, lgr=lgr, adapter=adapter)
+
+    def read(self, rdbyte=0):
+        """Read data ultility"""
+        rdbuf = ""
+        rdbuf = self.dev.recv(RAWNODE_RBUF_MAX).decode()
+        print("SVR:", rdbuf)
+        sender, data = rdbuf.split(":")
+        print(self.dev.sendto(data.encode(), socket.MSG_DONTWAIT, sender))
+        return rdbuf
+
+    def write(self, data="", request=None):
+        """Write data ultility"""
+        pass
+#        written = 0
+#        if isinstance(self.dev, socket.socket): #DEBUG
+#            self.dev.sendto(data, socket.MSG_DONTWAIT, RAWNODE_SVR)
+#        #TODO -->
+#        self.report_write(data, request)
+#        if self.run_listener:
+#            self.read_event.set()
 
 if __name__ == '__main__':
     lgr = logging.getLogger('InuithyNode')
-
+    node = SerialNode(path='/dev/tty2')
+    node.close()
