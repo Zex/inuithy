5e1db912c5d8952f4aedcdc8a758eceeb28b33f6
Zex Li <top_zlynch@yahoo.com>
2016-12-16 18:01:26 +0800

    add with_child, remove tuple args

--- inuithy/util/task_manager.py
+++ inuithy/util/task_manager.py
@@ -3,11 +3,16 @@
 """
 from inuithy.common.predef import to_string, INUITHY_LOGCONFIG,\
 T_EVERYONE
+from inuithy.util.helper import clear_list
 import os
 import multiprocessing as mp
 import threading
 import logging.config as lconf
 import logging
+try:
+    from queue import Queue, Empty
+except ImportError:
+    from Queue import Queue, Empty
 
 lconf.fileConfig(INUITHY_LOGCONFIG)
 logger = logging.getLogger('TaskManager')
@@ -15,33 +20,36 @@ logger = logging.getLogger('TaskManager')
 class ProcTaskManager(object):
     """Process task manager
     """
-    def __init__(self, lgr=None):
-        self.__tasks = []
-        self.lgr = lgr
-        if self.lgr is None:
-           self.lgr = logging
+    def __init__(self, lgr=None, with_child=False):
+        self.__tasks = [] #self.shared_mng.Queue()
+        self.lgr = lgr is None and logging or lgr
+        self.with_child = with_child
 
     def waitall(self):
         self.lgr.info(to_string('[{}] tasks running', len(self.__tasks)))
         try:
-            [os.waitpid(t.pid, 0) for t in self.__tasks if t.is_alive()]
+            [t.join() for t in self.__tasks if t.is_alive()]
             self.lgr.info(to_string('[{}] tasks finished', len(self.__tasks)))
+            clear_list(self.__tasks)
         except Exception as ex:
             self.lgr.error(to_string("Exception on to waiting all: {}", ex))
 
-    def create_task(self, proc, args=()):
+    def create_task(self, proc, *args):
         """Create one task with args
         """
         try:
             t = mp.Process(target=proc, args=args)
             self.__tasks.append(t)
-            t.start()
-#            self.lgr.info(to_string('[{}]/{} running', t.pid, len(self.__tasks)))
+            if self.with_child:
+                t.start()
+            else:
+                t.run()
         except Exception as ex:
             self.lgr.error(to_string("Create task with [{}] failed: {}", args, ex))
 
     def create_task_foreach(self, proc, objs, *args):
         """Create task foreach object with args
+        foreach `obj` in `objs` execute `proc` with `args`
         """
         self.lgr.info("Tasks creation begin")
 #        [self.create_task(proc, (o, *args)) for o in objs]
@@ -53,26 +61,24 @@ class ThrTaskManager(object):
     """
     def __init__(self, lgr=None):
         self.__tasks = []
-        self.lgr = lgr
-        if self.lgr is None:
-           self.lgr = logging
+        self.lgr = lgr is None and logging or lgr
 
     def waitall(self):
         self.lgr.info(to_string('[{}] tasks running', len(self.__tasks)))
         try:
             [t.join() for t in self.__tasks if t.isAlive()]
             self.lgr.info(to_string('[{}] tasks finished', len(self.__tasks)))
+            clear_list(self.__tasks)
         except Exception as ex:
             self.lgr.error(to_string("Exception on to waiting all: {}", ex))
 
-    def create_task(self, proc, args=()):
+    def create_task(self, proc, *args):
         """Create one task with args
         """
         try:
             t = threading.Thread(target=proc, args=args)
             self.__tasks.append(t)
             t.run()
-#            self.lgr.info(to_string('[{}]/{} running', t.name, len(self.__tasks)))
         except Exception as ex:
             self.lgr.error(to_string("Create task with [{}] failed: {}", args, ex))
 
