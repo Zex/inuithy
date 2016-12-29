c5443d092b9c2b7d644511cd737909c27ef440e9
Zex Li <top_zlynch@yahoo.com>
2016-12-29 19:15:07 +0800

    pack for deployment

--- makefiles/pack.mk
+++ makefiles/pack.mk
@@ -14,7 +14,8 @@ $(error "VERSION_PATH not defined")
 endif
 
 PROJECT_ALIAS		   := $(shell tr A-Z a-z <<< $(PROJECT))
-OUTPUT_TAR	           := $(strip $(PROJECT_ALIAS))-$(strip $(VERSION)).tar.bz2
+OUTPUT_TAR_BASE        := $(strip $(PROJECT_ALIAS))-$(strip $(VERSION))
+OUTPUT_TAR	           := $(OUTPUT_TAR_BASE).tar.bz2
 OUTPUT_TAR_SOURCE	   := inuithy thirdparty tools Makefile makefiles
 OUTPUT_TAR_PATH        := $(BUILD)/$(OUTPUT_TAR)
 OUTPUT_LOGTAR          := $(strip $(PROJECT_ALIAS))-log-$(shell date +'%d%m%Y-%H%M').tar.bz2
@@ -22,11 +23,24 @@ OUTPUT_LOGTAR_PATH     := $(BUILD)/$(OUTPUT_LOGTAR)
 OUTPUT_TAR_EXCLUDES	   := $(BUILD) *.pyc .git .svn .*.swp docs *~ *.cache *__pycache__*
 PYLINT_OUTPUT		   := $(BUILD)/pylint
 DEPLOY_SH	   		   := deploy_on_board.sh
-OUTPUT_DEPLOY_SH	   := $(BUILD)/$(DEPLOY_SH)
+INSTALL_BOARD_SH	   := install_board.sh
+OUTPUT_INSTALL_BOARD_SH:= tools/$(INSTALL_BOARD_SH)
+OUTPUT_DEPLOY_SH	   := tools/$(DEPLOY_SH)
+OUTPUT_DEPEND_TAR	   := deploy_env.tar.bz2
+OUTPUT_DEPEND		   := $(BUILD)/$(OUTPUT_DEPEND_TAR)
+DEPLOY_ENV_SH	   	   := tools/deploy_env.sh
+OUTPUT_DEPEND_SOURCE   := deploy
 
 $(OUTPUT_TAR_PATH): $(BUILD) $(VERSION_PATH)
 	$(ECHO) "\033[01;36m[Creating $@]\033[00m"
-	$(TAR) cfj $(OUTPUT_TAR_PATH) $(OUTPUT_TAR_SOURCE) $(OUTPUT_TAR_EXCLUDES:%=--exclude=%) --owner=root --group=root --total -l
+	$(RM) $(OUTPUT_INSTALL_BOARD_SH)
+	make $(OUTPUT_INSTALL_BOARD_SH)
+	$(RM) $(OUTPUT_DEPLOY_SH)
+	make $(OUTPUT_DEPLOY_SH)
+	$(MKDIR) $(OUTPUT_TAR_BASE)
+	$(CP) $(OUTPUT_TAR_SOURCE) $(OUTPUT_TAR_BASE)
+	$(TAR) cfj $(OUTPUT_TAR_PATH) $(OUTPUT_TAR_BASE) $(OUTPUT_TAR_EXCLUDES:%=--exclude=%) --owner=root --group=root --total -l
+	$(RM) $(OUTPUT_TAR_BASE)
 
 $(VERSION_PATH):
 	$(ECHO) "\033[01;36m[Creating $@]\033[00m"
@@ -40,7 +54,8 @@ $(VERSION_PATH):
 	$(ECHO) "PROJECT = \""$(PROJECT_ALIAS)"\"" >> $@
 	$(ECHO) "DEPLOY_SH = \""$(DEPLOY_SH)"\"" >> $@
 	$(ECHO) "PROJECT_PATH = \""$(PROJECT_PATH)"\"" >> $@
-	$(ECHO) "INUITHY_ROOT = \""$(PROJECT_PATH)/inuithy"\"" >> $@
+	$(ECHO) "INUITHY_ROOT = '{}/{}'.format(PROJECT_PATH, PROJECT)" >> $@
+	$(ECHO) "INUITHY_AGENT_INTERPRETER = \""$(PYTHON)"\"" >> $@
 	$(ECHO) "" >> $@
 
 grab_logs: $(BUILD)
@@ -51,13 +66,18 @@ $(OUTPUT_DEPLOY_SH):
 	$(ECHO) "\033[01;36m[Creating $@]\033[00m"
 	$(ECHO) "# Inuithy deploy script - AUTO GENERATED ON `date`" > $@
 	$(ECHO) "" >> $@
+	$(ECHO) "[ ! $(AGENT_ARCH) == \`uname -m\` ] && exit 0" >> $@
+	$(ECHO) "# Reference to $(DEPLOY_ENV_SH)" >> $@
+	$(CAT) >> $@ < $(DEPLOY_ENV_SH)
+	$(ECHO) "" >> $@
 	$(ECHO) "latest_version=inuithy-$(VERSION)" >> $@
-	$(ECHO) -e "latest_pack=\$$latest_version.tar.bz2" >> $@
+	$(ECHO) "latest_pack=\$$latest_version.tar.bz2" >> $@
 	$(ECHO) "dest_base=/opt" >> $@
 	$(ECHO) "tmp_base=opt/inuithy" >> $@
-	$(ECHO) "" >> $@
+	$(ECHO) "# $(DEPLOY_ENV_SH) END" >> $@
 	$(ECHO) "pushd /media/card > /dev/null" >> $@
 	$(ECHO) "" >> $@
+	$(ECHO) "env_chk" >> $@
 	$(ECHO) "if [ ! -f \$$latest_pack ]" >> $@
 	$(ECHO) "then" >> $@
 	$(ECHO) "    echo "\$$latest_pack not found"" >> $@
@@ -66,8 +86,8 @@ $(OUTPUT_DEPLOY_SH):
 	$(ECHO) "" >> $@
 	$(ECHO) "if [ -d \$$tmp_base ] ;then rm -rf \$$tmp_base; fi" >> $@
 	$(ECHO) "" >> $@
-	$(ECHO) "mkdir -p \$$tmp_base" >> $@
-	$(ECHO) "tar xf \$$latest_pack --no-same-owner -C \$$tmp_base 1> /dev/null 2> /tmp/inuithy.update" >> $@
+	$(ECHO) "tar xf \$$latest_pack --no-same-owner 1> /dev/null 2> /tmp/inuithy.update" >> $@
+	$(ECHO) "mv $(OUTPUT_TAR_BASE) \$$tmp_base" >> $@
 	$(ECHO) "pushd \$$tmp_base > /dev/null" >> $@
 	$(ECHO) "make preset 1> /dev/null 2>> /tmp/inuithy.update" >> $@
 	$(ECHO) "popd > /dev/null" >> $@
@@ -78,5 +98,29 @@ $(OUTPUT_DEPLOY_SH):
 	$(ECHO) "fi" >> $@
 	$(ECHO) "" >> $@
 	$(ECHO) "popd > /dev/null" >> $@
+	$(ECHO) "rm -rf \$$latest_pack" >> $@
+	$(ECHO) "rm -rf $(OUTPUT_DEPEND_TAR) $(OUTPUT_DEPEND_SOURCE)" >> $@
 	$(CHMOD) +x $@
+
+$(OUTPUT_DEPEND):
+	$(ECHO) "\033[01;36m[Creating $@]\033[00m"
+	$(TAR) cfj $@ $(OUTPUT_DEPEND_SOURCE) $(OUTPUT_TAR_EXCLUDES:%=--exclude=%) --owner=root --group=root --total -l
+
+$(OUTPUT_INSTALL_BOARD_SH):
+	$(ECHO) "\033[01;36m[Creating $@]\033[00m"
+	$(ECHO) "# Installer for Inuithy Agent - AUTO GENERATED ON `date`" > $@
+	$(ECHO) "" >> $@
+	$(ECHO) "pack_base=.." >> $@
+	$(ECHO) "srcs=\"\$$pack_base/$(OUTPUT_DEPEND_TAR) tools/$(DEPLOY_SH) \$$pack_base/$(OUTPUT_TAR)\"" >> $@
+	$(ECHO) "if [ 1 -gt \$$# ];then echo "No hosts given"; exit 0; fi" >> $@
+	$(ECHO) "dests=\$$@" >> $@
+	$(ECHO) "for dest in \$$dests; do" >> $@
+	$(ECHO) "	echo -e \"\033[01;31m[Sending to \$$dest]\033[00m\"" >> $@
+	$(ECHO) "	ssh root@\$$dest \"cd /media/card/;rm -rf deploy $(OUTPUT_DEPEND_TAR) $(DEPLOY_SH) $(OUTPUT_TAR)\"" >> $@
+	$(ECHO) "	scp -r \$$srcs root@\$$dest:/media/card" >> $@
+	$(ECHO) "	echo -e \"\033[01;34m[Deploying Inuithy on \$$dest]\033[00m\"" >> $@
+	$(ECHO) "	ssh root@\$$dest /media/card/$(DEPLOY_SH)" >> $@
+	$(ECHO) "done" >> $@
+	$(CHMOD) +x $@
+	
 
